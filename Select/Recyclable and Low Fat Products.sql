11aa00d098761ada8c2bc147220a67339dd08e2e
Sanjay M <sanjay.m192004@gmail.com>
2025-08-09 11:49:06 +0530

    MySQL Solution

--- Select/Recyclable and Low Fat Products.sql
+++ Select/Recyclable and Low Fat Products.sql
@@ -1,1 +1,19 @@
+/*
+Problem: Recyclable and Low Fat Products
+Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
+
+Description:
+Find all products that are both recyclable and low fat from the Products table.
+
+Approach:
+Filter products where 'recyclable' = 'Y' and 'low_fats' = 'Y'.
+
+Solution:
+*/
+
+SELECT 
+    product_id
+FROM products
+WHERE low_fats = 'Y' AND
+      recyclable = 'Y' ;
 
